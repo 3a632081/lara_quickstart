b6b9b80ef35213804e106187509b99b3e9c6f92e
3a632081 <s3a632081@student.ncut.edu.tw>
2019-10-21 18:15:23 +0800

    第一個路由：匿名函數將回傳tasks 視圖(view)，以顯示任務清單網頁

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use App\Task;
 use Illuminate\Http\Request;
 //顯示所有任務的清單
 Route::get('/', function () {
-    //
+    return view('tasks');
 });
 // 增加新的任務
 Route::post('/task', function (Request $request) {
@@ -28,4 +28,4 @@ Route::post('/task', function (Request $request) {
 // 刪除任務
 Route::delete('/task/{task}', function (Task $task) {
     //
-}); 
+});
